2408299b786fc2f0803d1376031dc878e70772cf
CarlosWylliam <99105323+carlos-wylliam@users.noreply.github.com>
2024-04-08 15:30:14 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+    return <h1>Eu odeio a chuva.
+    Neste mundo também chove.
+    Quando você enfrenta problemas, o céu se fecha.
+    Quando está triste, chove fácil.
+    Você compreende…
+    …o medo de se molhado pela chuva
+    em um mundo solitário desses?
+    Pra parar aquela chuva,
+    vou emprestar a você todo o poder que precisa.
+    Se você acreditar em mim, não vou deixar
+    cair um pingo de chuva neste mundo!
+    Acredite!
+    Você não é o único que está lutando!</h1>
+}
+
+export default Home;
